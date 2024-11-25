d78a86040b831f0b4ccb638cc8ab2cf348b4066e
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2024-11-25 13:26:04 +0100

    t6

--- my-node-server/public/task6.js
+++ my-node-server/public/task6.js
@@ -1,7 +1,14 @@
-const calcNum = (a, b) => {
-  sum = a + b;
-  console.log(sum);
-};
+// const calcNum = (a, b) => {
+//   sum = a + b;
+//   console.log(sum);
+// };
+
+function encremen(a, s) {
+  return a / s;
+}
+
+const result = encremen(80, 5);
+console.log(result);
 
 // function getRandomNum(arr) {
 //   let total = Math.random;
@@ -11,13 +18,13 @@ const calcNum = (a, b) => {
 //   }
 // }
 // getRandomNum(num);
-const num = [2, 5, 8, 7, 8, 3, 6];
-const arrLength = num.length;
+// const num = [2, 5, 8, 7, 8, 3, 6];
+// const arrLength = num.length;
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-console.log(getRandomInt(arrLength));
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
+// console.log(getRandomInt(arrLength));
 
 // console.log(getRandomInt(3));
 // // Expected output: 0, 1 or 2
@@ -30,3 +37,31 @@ console.log(getRandomInt(arrLength));
 
 // console.log(Math.random() * arrLength * 100);
 // Expected output: a number from 0 to <1
+
+// let array = ['ap', 're', 'sq'];
+// function nameAr(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// nameAr(array);
+
+// function count(a, d, f) {
+//   sum = a + d / f;
+//   console.log(sum);
+// }
+
+// count(234, 87, 3);
+
+// function seyHello(text, age = 18) {
+//   console.log(`Hello mst ${text} , you allready ${age} years old`);
+// }
+
+// seyHello('Vova');
+
+function makeMessage(username) {
+  console.log(`Hello ${username}`);
+}
+
+makeMessage('Jacob');
